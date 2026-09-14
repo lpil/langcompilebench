5ea66c8c58f2473b15316043aa8100331a3f55c5
Louis Pilfold <louis@lpil.uk>
2026-09-14 19:51:04 +0100

    Remove underperforming benchmarks
    I am not confident these are good representations of the languages being
    measured.

--- benchmark_kotlin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo Cleaning
-gradle clean
-
-echo Building
-time gradle compileKotlin
--- benchmark_ocaml/build-bytecode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo Cleaning
-rm -f src/*.cmi src/*.cmx src/*.o src/*.cmo
-
-echo Building
-time ocamlc -c src/*.ml
